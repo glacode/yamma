4730d20b9a2010487bd56be9b72baf0104d53de7
Glauco Siliprandi <glaformetamath@gmail.com>
2022-12-03 19:05:41 +0100

    test: added test 'StepDerivation ax-mp wrong EHyps order' test: added test 'StepDerivation wrong EHyps order and missing single EHyp'

--- server/src/__test__/StepDerivation.test.ts
+++ server/src/__test__/StepDerivation.test.ts
@@ -25,6 +25,26 @@ test('StepDerivation ax-mp', () => {
 	expect(textEdit.newText).toEqual(expectedText);
 });
 
+test('StepDerivation ax-mp wrong EHyps order', () => {
+	const mmpSource =
+		'h1::test.1 |- ps\n' +
+		'h2::test.2 |- ( ps -> ph )\n' +
+		'3:2,1: |- ph\n' +
+		'qed:: |- ch';
+	const mmpParser: MmpParser = new MmpParser(mmpSource, mp2MmParser, new WorkingVars(kindToPrefixMap));
+	mmpParser.parse();
+	const mmpUnifier: MmpUnifier = new MmpUnifier(mmpParser, ProofMode.normal, 100);
+	mmpUnifier.unify();
+	const textEditArray: TextEdit[] = mmpUnifier.textEditArray;
+	const textEdit: TextEdit = textEditArray[0];
+	const expectedText =
+		'h1::test.1          |- ps\n' +
+		'h2::test.2          |- ( ps -> ph )\n' +
+		'3:1,2:ax-mp        |- ph\n' +
+		'qed::              |- ch\n';
+	expect(textEdit.newText).toEqual(expectedText);
+});
+
 test('StepDerivation ax-ext', () => {
 	const mmpSource =
 		'50:: |- ( A. x ( x e. y <-> x e. z ) -> y = z )\n' +
@@ -47,6 +67,28 @@ test('StepDerivation 3syl', () => {
 		'3::                |- ( &W2 -> &W3 )\n' +
 		'4::                |- ( &W3 -> ps )\n' +
 		'5::               |- ( ph -> ps )\n' +
+		'qed::a |- ch';
+	const mmpParser: MmpParser = new MmpParser(mmpSource, eqeq1iMmParser, new WorkingVars(kindToPrefixMap));
+	mmpParser.parse();
+	const mmpUnifier: MmpUnifier = new MmpUnifier(mmpParser, ProofMode.normal, 100);
+	mmpUnifier.unify();
+	const textEditArray: TextEdit[] = mmpUnifier.textEditArray;
+	const textEdit: TextEdit = textEditArray[0];
+	const expectedText =
+		'2::                 |- ( ph -> &W2 )\n' +
+		'3::                 |- ( &W2 -> &W3 )\n' +
+		'4::                 |- ( &W3 -> ps )\n' +
+		'5:2,3,4:3syl       |- ( ph -> ps )\n' +
+		'qed::a             |- ch\n';
+	expect(textEdit.newText).toEqual(expectedText);
+});
+
+test('StepDerivation wrong EHyps order and missing single EHyp', () => {
+	const mmpSource =
+		'2::                |- ( ph -> &W2 )\n' +
+		'3::                |- ( &W2 -> &W3 )\n' +
+		'4::                |- ( &W3 -> ps )\n' +
+		'5:4,2:               |- ( ph -> ps )\n' +
 		'qed::a |- ch';
 	const mmpParser: MmpParser = new MmpParser(mmpSource, eqeq1iMmParser, new WorkingVars(kindToPrefixMap));
 	mmpParser.parse();
