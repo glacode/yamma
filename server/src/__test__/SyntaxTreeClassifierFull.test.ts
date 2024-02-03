d5c4176febdba85de69dda042af8270036fac8a3
Glauco <glaformetamath@gmail.com>
2024-02-03 15:42:57 +0100

    Test: the unit test for the 'full' classifier has been improved

--- server/src/__test__/SyntaxTreeClassifierFull.test.ts
+++ server/src/__test__/SyntaxTreeClassifierFull.test.ts
@@ -5,12 +5,12 @@ import { WorkingVars } from '../mmp/WorkingVars';
 import { SyntaxTreeClassifierFull } from '../stepSuggestion/SyntaxTreeClassifierFull';
 import { kindToPrefixMap, opelcnMmParser } from './GlobalForTest.test';
 
-function buildRpnSyntaxTree(stringToParse: string) : string {
+function buildRpnSyntaxTree(stringToParse: string, maxLevel: number): string {
 	opelcnMmParser.grammar.lexer = new MmLexer(new WorkingVars(kindToPrefixMap));
 	const parser = new Parser(opelcnMmParser.grammar);
 	parser.feed(stringToParse);
 	const parseNode: ParseNode = parser.results[0];
-	const syntaxTreeClassifierFull: SyntaxTreeClassifierFull = new SyntaxTreeClassifierFull(3);
+	const syntaxTreeClassifierFull: SyntaxTreeClassifierFull = new SyntaxTreeClassifierFull(maxLevel);
 	const rpnSyntaxTree: string = syntaxTreeClassifierFull.classify(parseNode, opelcnMmParser);
 	return rpnSyntaxTree;
 }
@@ -18,11 +18,15 @@ function buildRpnSyntaxTree(stringToParse: string) : string {
 test("test SyntaxTreeClassifierFull", () => {
 	// opthg2 $p | - ((C e.V /\ D e.W ) ->
 	// 	(<.A , B >. = <.C , D >. <-> (A = C /\ B = D ) ) ) $ =
-	let rpnSyntaxTree = buildRpnSyntaxTree('|- ( ph -> ps )');
+	let rpnSyntaxTree = buildRpnSyntaxTree('|- ( ph -> ps )', 2);
 	expect(rpnSyntaxTree).toEqual('wff wff wi TOP');
-	rpnSyntaxTree = buildRpnSyntaxTree('|- ( ( ph -> ps ) -> ch )');
+	rpnSyntaxTree = buildRpnSyntaxTree('|- ( ph -> ps )', 3);
+	expect(rpnSyntaxTree).toEqual('wff wff wi TOP');
+	rpnSyntaxTree = buildRpnSyntaxTree('|- ( ph -> ps )', 1);
+	expect(rpnSyntaxTree).toEqual('wi TOP');
+	rpnSyntaxTree = buildRpnSyntaxTree('|- ( ( ph -> ps ) -> ch )', 3);
 	expect(rpnSyntaxTree).toEqual('wff wff wi wff wi TOP');
 	rpnSyntaxTree = buildRpnSyntaxTree('|- ( ( C e. V /\\ D e. W ) ->\n' +
-		'( <. A , B >. = <. C , D >. <-> ( A = C /\\ B = D ) ) )');
+		'( <. A , B >. = <. C , D >. <-> ( A = C /\\ B = D ) ) )', 3);
 	expect(rpnSyntaxTree).toEqual('wcel wcel wa wceq wa wb wi TOP');
 });
