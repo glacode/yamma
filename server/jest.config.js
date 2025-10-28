ad8a523668fa364443deb206aed3691ef8ef6306
Glauco <glaformetamath@gmail.com>
2025-10-28 22:47:19 +0100

    feat(server): increase test timeout and compile before tests
    Increases the Jest test timeout to 10 seconds to prevent tests from failing due to timeouts, and adds a compilation step to the test script.

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testTimeout: 10000,
   modulePathIgnorePatterns: ["<rootDir>/out/"],
   collectCoverage: false,
   collectCoverageFrom: ["./src/**"]
